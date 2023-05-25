6dea2df8e7b52d806cef8cbf058d63ea495a5a5e
DenilsonSM1 <luiz2017.lvmb@gmail.com>
2023-05-25 14:44:00 -0300

    Criando a requisição POST onde vou inserir os meus dados que tenho no banco

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,35 @@
-import Fastify from 'fastify';
+import Fastify, { FastifyRequest, FastifyReply } from 'fastify';
+import { Prisma, PrismaClient } from "@prisma/client";
+import { dog } from "@prisma/client";
+import cors from '@fastify/cors';
+import { request } from 'http';
 
-
-
+const prisma = new PrismaClient();
 const app = Fastify();
+app.register(cors, {
+    origin: "*",
+});
+
+app.post('/create', async (request: FastifyRequest, reply: FastifyReply) => {
+    const { id, name, age, description, vaccinated } = request.body as dog;
+    const dog = await prisma.dog.create({
+        data: {
+            id,
+            name,
+            age,
+            description,
+            vaccinated
+        },
+    });
+    reply.send('dog created')
+});
+
+
+
+
+
+
+
 const start = async () => {
     try {
         await app.listen({ port: 3333 });
