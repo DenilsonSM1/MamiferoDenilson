9647235e24c6b630bea0a68d0fae6ffbae7b7b8a
DenilsonSM1 <luiz2017.lvmb@gmail.com>
2023-05-25 14:37:00 -0300

    Teste de servidor

---
+++ src/server.ts
@@ -0,0 +1,16 @@
+import Fastify from 'fastify';
+
+
+
+const app = Fastify();
+const start = async () => {
+    try {
+        await app.listen({ port: 3333 });
+        console.log('Server listening at http://localhost:3333');
+    } catch (error) {
+        console.error('Something went wrong.');
+        process.exit(1);
+    }
+};
+
+start();
