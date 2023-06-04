869e38f01dae7b2f63ad5b5f12c9603b91534ab1
DenilsonSM1 <luiz2017.lvmb@gmail.com>
2023-06-04 12:53:11 -0300

    Neste commit, foi implementada a função handleAddDog que será chamada quando o usuário pressionar o botão Adicionar. Essa função cria um novo objeto newDog com os dados fornecidos pelo usuário, incluindo o ID que é incrementado a cada novo cão adicionado. Em seguida, é feita uma requisição POST para a URL http://localhost:3333/create utilizando a biblioteca axios para enviar o objeto newDog ao servidor. Caso a requisição seja bem-sucedida, é exibida uma mensagem de sucesso no console. Caso ocorra algum erro, é exibido uma mensagem de erro no console.

--- mobile/src/components/AddDogScreen.tsx
+++ mobile/src/components/AddDogScreen.tsx
@@ -10,13 +10,38 @@ interface Dog {
   vaccinated: boolean;
 }
 let nextId = 1;
+
+
 const AddDogScreen = () => {
 
   const [name, setName] = useState('');
   const [age, setAge] = useState('');
   const [description, setDescription] = useState('');
   const [vaccinated, setVaccinated] = useState(false);
-  
-};
 
+  const handleAddDog = async () => {
+    const newDog: Dog = {
+      id: nextId++,
+      name,
+      age: parseInt(age),
+      description,
+      vaccinated,
+    };
+  
+    try {
+      const response = await axios.post('http://localhost:3333/create', newDog);
+      console.log('Cão adicionado:', response.data);
+    } catch (error) {
+      console.error('Erro ao adicionar cão:', error);
+    }
+  };
+
+
+
+  return(
+    <View >
+      
+    </View>
+  )
+};
 export default AddDogScreen;
